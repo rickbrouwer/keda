77fb3b9daebb03591119e62cebc8cef69ff411a1
Rick Brouwer <rickbrouwer@gmail.com>
2025-06-07 21:22:38 +0200

    Update newrelic_scaler_test.go
    Signed-off-by: Rick Brouwer <rickbrouwer@gmail.com>

--- pkg/scalers/newrelic_scaler_test.go
+++ pkg/scalers/newrelic_scaler_test.go
@@ -54,11 +54,17 @@ var testNewRelicMetadata = []parseNewRelicMetadataTestData{
 	{map[string]string{"account": "0", "threshold": "100", "queryKey": "somekey", "noDataError": "false", "nrql": "SELECT average(cpuUsedCores) as result FROM K8sContainerSample WHERE containerName='coredns'"}, map[string]string{}, false},
 	{map[string]string{"account": "0", "threshold": "100", "queryKey": "somekey", "noDataError": "0", "nrql": "SELECT average(cpuUsedCores) as result FROM K8sContainerSample WHERE containerName='coredns'"}, map[string]string{}, false},
 	{map[string]string{"account": "0", "threshold": "100", "queryKey": "somekey", "noDataError": "1", "nrql": "SELECT average(cpuUsedCores) as result FROM K8sContainerSample WHERE containerName='coredns'"}, map[string]string{}, false},
+	// percentile query with float threshold
+	{map[string]string{"account": "0", "threshold": "25.5", "queryKey": "somekey", "nrql": "SELECT percentile(jobs_graphql_event_loop_lag, 99.5) FROM Metric WHERE service.name='jobs-graphql-staging' SINCE 150 seconds ago"}, map[string]string{}, false},
+	// percentile query with activationThreshold
+	{map[string]string{"account": "0", "threshold": "30.0", "activationThreshold": "10.5", "queryKey": "somekey", "nrql": "SELECT percentile(duration, 95) FROM Transaction WHERE appName='MyApp'"}, map[string]string{}, false},
 }
 
 var newrelicMetricIdentifiers = []newrelicMetricIdentifier{
 	{&testNewRelicMetadata[1], 0, "s0-new-relic"},
 	{&testNewRelicMetadata[1], 1, "s1-new-relic"},
+	{&testNewRelicMetadata[17], 0, "s0-new-relic"},
+	{&testNewRelicMetadata[18], 2, "s2-new-relic"},
 }
 
 func TestNewRelicParseMetadata(t *testing.T) {
@@ -103,4 +109,42 @@ func TestNewRelicGetMetricSpecForScaling(t *testing.T) {
 			t.Error("Wrong External metric source name:", metricName)
 		}
 	}
+}
+
+func TestToFloat64(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    interface{}
+		expected *float64
+		isNil    bool
+	}{
+		{"float64 input", 24.625, floatPtr(24.625), false},
+		{"string float input", "24.625", floatPtr(24.625), false},
+		{"string int input", "42", floatPtr(42.0), false},
+		{"invalid string", "not-a-number", nil, true},
+		{"nil input", nil, nil, true},
+		{"int input", 42, nil, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := toFloat64(tc.input)
+			if tc.isNil {
+				if result != nil {
+					t.Errorf("Expected nil, got %v", *result)
+				}
+			} else {
+				if result == nil {
+					t.Errorf("Expected %f, got nil", *tc.expected)
+				} else if *result != *tc.expected {
+					t.Errorf("Expected %f, got %f", *tc.expected, *result)
+				}
+			}
+		})
+	}
+}
+
+// Helper function
+func floatPtr(f float64) *float64 {
+	return &f
 }
